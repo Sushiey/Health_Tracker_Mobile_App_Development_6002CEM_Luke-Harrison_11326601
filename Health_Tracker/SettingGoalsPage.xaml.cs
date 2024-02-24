70eae45d8ced1294b8c735623a57f70ef6a63a96
Luke Daniel Harrison <luke87792@gmail.com>
2024-02-24 20:38:37 +0000

    Update SettingGoalsPage.xaml.cs

--- Health_Tracker/SettingGoalsPage.xaml.cs
+++ Health_Tracker/SettingGoalsPage.xaml.cs
@@ -9,14 +9,11 @@ namespace HealthTrackerApp
             InitializeComponent();
         }
 
-        // Event handler for the "Save Goals" button
         private void OnSaveGoalsClicked(object sender, EventArgs e)
         {
             if (int.TryParse(stepGoalEntry.Text, out int stepGoal) && int.TryParse(calorieGoalEntry.Text, out int calorieGoal))
             {
-                // Save the goals
-                // Replace this with your actual implementation to save goals data
-                // For example, you could use Xamarin.Essentials Preferences or a database
+
                 SaveGoals(stepGoal, calorieGoal);
             }
             else
@@ -25,12 +22,9 @@ namespace HealthTrackerApp
             }
         }
 
-        // Method to save the goals (replace with actual implementation)
         private void SaveGoals(int stepGoal, int calorieGoal)
         {
-            // Replace this method with your actual implementation to save goals data
-            // For example, you could use Xamarin.Essentials Preferences or a database
-            // This is just a placeholder
+
         }
     }
 }
