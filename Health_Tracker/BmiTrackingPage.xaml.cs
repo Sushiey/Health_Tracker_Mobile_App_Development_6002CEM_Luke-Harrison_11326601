e708d8ebf8a1642030e547ad2ac63ecf06fdd90c
Luke Daniel Harrison <luke87792@gmail.com>
2024-02-24 20:35:51 +0000

    Update BmiTrackingPage.xaml.cs

--- Health_Tracker/BmiTrackingPage.xaml.cs
+++ Health_Tracker/BmiTrackingPage.xaml.cs
@@ -9,17 +9,14 @@ namespace HealthTrackerApp
             InitializeComponent();
         }
 
-        // Event handler for the "Calculate BMI" button
         private void OnCalculateClicked(object sender, EventArgs e)
         {
             if (double.TryParse(weightEntry.Text, out double weight) &&
                 double.TryParse(heightEntry.Text, out double height) &&
                 height > 0)
             {
-                // Calculate BMI
                 double bmi = weight / (height * height);
 
-                // Display BMI
                 bmiLabel.Text = $"Your BMI is: {bmi:F2}";
             }
             else
