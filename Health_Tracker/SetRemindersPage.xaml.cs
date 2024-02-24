1b4ea6c75230c785e73070ce2079efa63ee1c9bf
Luke Daniel Harrison <luke87792@gmail.com>
2024-02-24 20:38:19 +0000

    Update SetRemindersPage.xaml.cs

--- Health_Tracker/SetRemindersPage.xaml.cs
+++ Health_Tracker/SetRemindersPage.xaml.cs
@@ -12,16 +12,12 @@ namespace HealthTrackerApp
 
         private void OnSaveReminderClicked(object sender, EventArgs e)
         {
-            // Get the values entered by the user
             string title = TitleEntry.Text;
             DateTime date = DatePicker.Date;
             TimeSpan time = TimePicker.Time;
-
-            // Combine date and time to create the reminder datetime
+            
             DateTime reminderDateTime = date.Date + time;
 
-            // Here, you can handle saving the reminder, such as adding it to a list or storing it in a database
-            // For now, let's just display a message
             DisplayAlert("Reminder Set", $"Reminder Title: {title}\nReminder DateTime: {reminderDateTime}", "OK");
         }
     }
