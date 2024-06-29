0055450f98722b3e4f0f3f1cb5df77b33f9b8b88
Luke Daniel Harrison <luke87792@gmail.com>
2024-06-29 19:18:49 +0100

    Revert "Big Updated Activity Tracker"
    This reverts commit c9055d55e7cdda1f36a6ba5419c1b9e6154c6ea7.

--- Health_Tracker/ActivityTrackingPage.xaml.cs
+++ Health_Tracker/ActivityTrackingPage.xaml.cs
@@ -1,60 +1,26 @@
 ﻿using Microsoft.Maui.Controls;
 using System;
-using Health_Tracker.Data;
-using Health_Tracker;
 
 namespace HealthTrackerApp
 {
     public partial class ActivityTrackingPage : ContentPage
     {
-        private readonly ActivityRepository _activityRepository;
-
         public ActivityTrackingPage()
         {
             InitializeComponent();
-
-            string dbPath = AppDatabase.GetDbPath();
-            var db = new AppDatabase(dbPath);
-            _activityRepository = new ActivityRepository(dbPath);
         }
 
-        private async void OnDateSelected(object sender, DateChangedEventArgs e)
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
         {
             selectedDateLabel.Text = "Selected Date: " + e.NewDate.ToString("MM/dd/yyyy");
-
-            // Load activity for the selected date
-            var activity = await _activityRepository.GetActivityByDateAsync(e.NewDate);
-            if (activity != null)
-            {
-                stepEntry.Text = activity.Steps.ToString();
-                stepCountLabel.Text = "Step count for selected date: " + activity.Steps.ToString();
-            }
-            else
-            {
-                stepEntry.Text = string.Empty;
-                stepCountLabel.Text = "Step count for selected date: Not specified";
-            }
         }
 
-        private async void OnEditClicked(object sender, EventArgs e)
+        private void OnEditClicked(object sender, EventArgs e)
         {
             string stepCountText = stepEntry.Text;
             if (!string.IsNullOrEmpty(stepCountText))
             {
-                int steps = Convert.ToInt32(stepCountText);
-
-                // Save or update activity for the selected date
-                var selectedDate = datePicker.Date;
-                var activity = new Activity
-                {
-                    Date = selectedDate,
-                    Steps = steps
-                };
-
-                await _activityRepository.SaveActivityAsync(activity);
-
-                // Update step count label
-                stepCountLabel.Text = "Step count for selected date: " + steps.ToString();
+                stepCountLabel.Text = "Step count for selected date: " + stepCountText;
             }
             else
             {
